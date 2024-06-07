75c4837c8284e066638899793f14a8a5525b339b
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-06-07 18:25:40 +0200

    sing_up form

---
+++ lib/sing_up.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyForm extends StatelessWidget {
+  const MyForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Form'),
+      ),
+      body: Form(
+          child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0))),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0))),
+          ),
+          SizedBox(height: 16.0),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Phone Number',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0))),
+          ),
+          SizedBox(height: 16.0),
+          Container(
+            height: 50,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit'),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
