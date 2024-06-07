4308174945ca8e75ddfe8c6c50e7ae7d5ca146eb
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-06-07 20:08:50 +0200

    singn Up ui finished

--- lib/sing_up.dart
+++ lib/sing_up.dart
@@ -1,50 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class MyForm extends StatelessWidget {
-  const MyForm({super.key});
+class SignUpView extends StatelessWidget {
+  SignUpView({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitForm(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Form Submitted successfully')),
+      );
+    }
+  }
+
+  String? _validateEmail(value) {
+    if (value!.isEmpty) {
+      return 'Please enter an email';
+    }
+    RegExp emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _validatePhoneNumber(value) {
+    if (value!.isEmpty) {
+      return 'Please enter a phone number';
+    }
+    if (value.length != 9) {
+      return 'Please enter a 9-digit phone number';
+    }
+    return null;
+  }
+
+  String? _validatePassword(value) {
+    if (value!.isEmpty) {
+      return 'Please enter a password';
+    }
+    return null;
+  }
+
+  String? _validateUserName(value) {
+    if (value!.isEmpty) {
+      return 'Please enter a username';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Form'),
-      ),
-      body: Form(
+      backgroundColor: Color(0xFF252634),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
           child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
+            children: [
+              SizedBox(height: 80.0),
+              SizedBox(
+                width: 250,
+                child: Text(
+                  'Create New Account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 50.0),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: _buildInputDecoration('Username', Icons.person),
+                validator: _validateUserName,
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Colors.white),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: _buildInputDecoration('Email', Icons.email),
+                validator: _validateEmail,
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                style: TextStyle(color: Colors.white),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: _buildInputDecoration('Phone number', Icons.call),
+                validator: _validatePhoneNumber,
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                style: TextStyle(color: Colors.white),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: _buildInputDecoration('Password', Icons.lock),
+                validator: _validatePassword,
+              ),
+              SizedBox(height: 40.0),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 241, 89, 0),
+                  ),
+                  onPressed: () => _submitForm(context),
+                  child: Text('Create'),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Color(0xFFF15900), fontSize: 20),
+                ),
+              )
+            ],
           ),
-          SizedBox(
-            height: 16.0,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
-          ),
-          SizedBox(height: 16.0),
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Phone Number',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
-          ),
-          SizedBox(height: 16.0),
-          Container(
-            height: 50,
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('Submit'),
-            ),
-          )
-        ],
-      )),
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _buildInputDecoration(String label, IconData suffixIcon) {
+    return InputDecoration(
+      fillColor: Color(0xAA494A59),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color(0x35949494)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      filled: true,
+      labelStyle: TextStyle(color: Color(0xFF949494)),
+      labelText: label,
+      suffixIcon: Icon(
+        suffixIcon,
+        color: Color(0xFF949494),
+      ),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
